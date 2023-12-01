08ce1bc2ea0eed2670aad9ca9a1ee9f111d12b9b
jycouet <jycouet@gmail.com>
2023-12-01 12:36:42 +0100

    add kit routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,15 @@
 <h2>Home</h2>
 
 <ul>
-  <li>1️⃣ npm create svelte@latest demo-kit-routes</li>
-  <li>2️⃣ Add a Layout (1 reset CSS + title)</li>
-  <li>3️⃣ Add an about page</li>
+  <li>1️⃣ npm create <span class="wow">svelte</span>@latest demo-kit-routes</li>
+  <li>2️⃣ Added an about page</li>
+  <li>3️⃣ Added a Layout (1 reset CSS + title + menu)</li>
+  <li>4️⃣ pnpm add <span class="wow">vite-plugin-kit-routes</span> -D</li>
+  <li>👉 WE ARE HERE</li>
 </ul>
+
+<style>
+  .wow {
+    color: #ff3e00;
+  }
+</style>
