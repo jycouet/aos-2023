5371ea021af5cbea3c204fd135df4a35295d419c
jycouet <jycouet@gmail.com>
2023-12-01 16:43:20 +0100

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
-<h2>Home</h2>
+<h2>Slides of the demo</h2>
 
 <ul>
   <li>1️⃣ npm create <span class="wow">svelte</span>@latest demo-kit-routes</li>
-  <li>2️⃣ Added an info page</li>
-  <li>3️⃣ Added a Layout (1 reset CSS + title + menu)</li>
+  <li>2️⃣ Added a +layout.svelte (1 reset CSS + title + menu)</li>
+  <li>3️⃣ Added an info/+page.svelte</li>
   <li>4️⃣ pnpm add <span class="wow">vite-plugin-kit-routes</span> -D</li>
   <li>👉 WE ARE HERE</li>
 </ul>
