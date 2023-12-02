05b59e56cf67b0bfa254e819b0c0122665843e71
jycouet <jycouet@gmail.com>
2023-12-02 20:13:35 +0100

    :construction: WIP: logs

--- src/routes/aos/day-02/+page.server.ts
+++ src/routes/aos/day-02/+page.server.ts
@@ -4,6 +4,9 @@ export const actions = {
   check: async ({ request }) => {
     const data = await request.formData();
     const guess = data.get("guess");
+
+    console.log("Guessing... ", guess);
+
     return guess === "7"
       ? `Correct number of 🍪 you found well the number 7`
       : `${guess}! Incorrect, check out Cookies 😉`;
